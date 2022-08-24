fb3eb3deac30073aba91375c0a7e4c1662240193
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-08-24 15:10:55 -0700

    Adds allowed host for deployment

--- codedex/settings.py
+++ codedex/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('MODE') == 'dev' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codedex.herokuapp.com']
 
 
 # Application definition
