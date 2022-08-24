245e2db4e405a68ed9671b24f20c7997401659ac
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-08-24 16:10:18 -0700

    testing template inheritance

--- code_dex/views.py
+++ code_dex/views.py
@@ -3,4 +3,4 @@ from django.views.generic.base import TemplateView
 
 # Create your views here.
 class Landing(TemplateView):
-   template_name = 'code_dex/test.html'
+   template_name = 'code_dex/testchild.html'
