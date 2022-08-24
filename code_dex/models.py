e1754669bf9b36111e140f259978512cc29ce60a
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-08-24 15:47:28 -0700

    Fixes syntax for model

--- code_dex/models.py
+++ code_dex/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Record(models.Model):
-   owner = models.ForeignKey(User, on_delete=models.CASCADE)
+   owner = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
    category = models.CharField(max_length=50)
    file = models.FileField()
